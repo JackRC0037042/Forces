5fe6ffe6e96d5edc03cf524b0f403557b6e9f03e
Jack Cross <rc0037042@student.riversidecollege.ac.uk>
2020-03-03 12:19:05 +0000

    Add project files.

---
+++ Forces/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Forces
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        double sin(double x)
+        {
+            return (Math.Sin(x * Math.PI / 180.0));
+        }
+
+        double asin(double x)
+        {
+            return (Math.Asin(x) * 180 / Math.PI);
+        }
+        double cos(double x)
+        {
+            return (Math.Cos(x * Math.PI / 180.0));
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            // Read values from textboxes
+            double Force = double.Parse(textBox1.Text);
+            double Angle = double.Parse(textBox2.Text);
+
+            //Calculate Fx and Fy
+            double Fx = Force * cos(Angle);
+            double Fy = Force * sin(Angle);
+
+            //put values for Fx and Fy in labels
+            label1.Text = "Fx = " + Fx + " N";
+            label2.Text = "Fy = " + Fy + " N";
+        }
+
+    }
+}
